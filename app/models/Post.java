0dacf673f499df8f788660cd4add6dcc5f859f9b
Mingoo Kim <bearchit@gmail.com>
2012-05-26 15:11:35 +0900

    모델 테스트 작성

--- app/models/Post.java
+++ app/models/Post.java
@@ -8,27 +8,28 @@ import play.db.jpa.*;
 @Entity
 public class Post extends Model {
 
-  @ManyToOne
-  public User author;
+	@ManyToOne
+	public User author;
 
-  @Lob
-  public String content;
+	@Lob
+	public String content;
 
-  @OneToMany(mappedBy="post", cascade=CascadeType.ALL)
-  public List<Comment> comments;
+	@OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+	public List<Comment> comments;
 
-  public Date createdAt;
+	public Date createdAt;
 
-  public Post(User author, String content) {
-    this.author = author;
-    this.content = content;
-    this.createdAt = new Date();
-  }
+	public Post(User author, String content) {
+		this.comments = new ArrayList<Comment>();
+		this.author = author;
+		this.content = content;
+		this.createdAt = new Date();
+	}
 
-  public Post addComment(User author, String content) {
-    Comment newComment = new Comment(this, author, content).save();
-    this.comments.add(newComment);
-    this.save();
-    return this;
-  }
+	public Post addComment(User author, String content) {
+		Comment newComment = new Comment(this, author, content).save();
+		this.comments.add(newComment);
+		this.save();
+		return this;
+	}
 }
